f10f99a8c37db4f53b5e17597a744fcee2dd179e
OkezueVictor <victorokezue112@gmail.com>
2024-08-07 04:11:34 -0700

    Updated

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ else:
 if len (filtered_table) > 0:
     total_qty = filtered_table['quantity'].sum()
 else:
-    total_sales = df.quantity.sum()
+    total_qty = df.quantity.sum()
 
 # total_transactions = df.Id.count()
 if len (filtered_table) > 0:
@@ -93,9 +93,9 @@ try:
         daily_sales = filtered_table.groupby('date')['sales'].sum()
     else:
         daily_sales = df.groupby('date')['sales'].sum()
-        
+
     daily_sales_df = daily_sales.reset_index().rename(columns={'sales':'total sales'})
-    st.area_chart(ax)(daily_sales_df,
+    st.area_chart(daily_sales_df,
                       x='date',
                       y='total sales')
 except ValueError as e:
